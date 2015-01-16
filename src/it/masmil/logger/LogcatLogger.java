35d854701b556a23eca0ff5645e8bd0f970f508e
Massimo Milazzo <massimo1.milazzo@guest.telecomitalia.it>
2015-01-16 13:00:04 +0100

    Simplified logger. Solved bug on increasing memory request.

--- src/it/masmil/logger/LogcatLogger.java
+++ src/it/masmil/logger/LogcatLogger.java
@@ -23,7 +23,7 @@ public class LogcatLogger {
 	private AtomicBoolean mStarted = new AtomicBoolean(false);
 
 	private static final String COMMAND_CLEAR_LOGCAT = "logcat -c";
-	private static final String COMMAND_LOG_LOGCAT = "logcat -v time";
+	private static final String COMMAND_LOG_LOGCAT = "logcat -v time -n 1";
 	private static final String LOGCAT_TIME_FORMAT = "MM-dd HH:mm:ss.SSS";
 	private static final String LOGCAT_TIME_FORMAT_WITH_YEAR = "yyyy-"+LOGCAT_TIME_FORMAT;
 	private static final String LOGCAT_TIME_PATTERN = "([0-9]{2}\\-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\\.[0-9]{3})";
@@ -46,7 +46,7 @@ public class LogcatLogger {
 	public void startLogging() {
 		if (mStarted.getAndSet(true))
 			return;
-		
+
 		Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -57,7 +57,7 @@ public class LogcatLogger {
 				} catch (IOException e) {
 					return;
 				}
-				
+
 				long lastTimestamp = System.currentTimeMillis();
 				Process process;
 				try {
@@ -67,64 +67,55 @@ public class LogcatLogger {
 				}
 				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-				StringBuilder logBuilder=new StringBuilder();
-
 				HashSet<String> logsForTimestamp = new HashSet<String>();
 				while (mStarted.get()) {
-					String lineRead;
+					String line;
 					try {
-						lineRead = bufferedReader.readLine();
+						line = bufferedReader.readLine();
 					} catch (IOException e) {
 						continue;
 					}
-					
-					logBuilder.append("\n"+lineRead);
-					String toPrint = logBuilder.toString().trim();
 
-					if (toPrint.length() > 0) {
-						String[] lines = toPrint.split("\\n");
+					if (line.length() > 0) {
+						for (String tag: mTags) {
+							String patternToMatch = LOGCAT_TIME_PATTERN+" (.)/"+tag+" *\\(.+\\): (.*)$";
+							Pattern pattern = Pattern.compile(patternToMatch);
+							Matcher matcher = pattern.matcher(line);
+							if (matcher.find()) {
+								String time = Calendar.getInstance().get(Calendar.YEAR)+"-"+matcher.group(1);
+								String logType = matcher.group(2);
+								String log = matcher.group(3);
 
-						for (String line: lines) {
-							for (String tag: mTags) {
-								String patternToMatch = LOGCAT_TIME_PATTERN+" (.)/"+tag+" *\\(.+\\): (.*)$";
-								Pattern pattern = Pattern.compile(patternToMatch);
-								Matcher matcher = pattern.matcher(line);
-								if (matcher.find()) {
-									String time = Calendar.getInstance().get(Calendar.YEAR)+"-"+matcher.group(1);
-									String logType = matcher.group(2);
-									String log = matcher.group(3);
-									
-									Date d;
+								Date d;
+								try {
+									d = mDateFormatter.parse(time);
+								} catch (ParseException e) {
+									d = new Date();
+								}
+								long timestamp = d.getTime();
+								String[] splittedParts = line.split(".*"+tag+" *\\(.+\\): ");
+								if (splittedParts.length > 0)
+									line = splittedParts[1];
+								else
+									line = "";
+
+								boolean takeLog = false;
+
+								if (lastTimestamp < timestamp) {
+									lastTimestamp = timestamp;
+									logsForTimestamp.clear();
+									logsForTimestamp.add(log);
+									takeLog = true;
+								} else if (lastTimestamp == timestamp && !logsForTimestamp.contains(log)) {
+									logsForTimestamp.add(log);
+									takeLog = true;
+								}
+
+								if (takeLog && mLogListener != null) {
 									try {
-										d = mDateFormatter.parse(time);
-									} catch (ParseException e) {
-										d = new Date();
-									}
-									long timestamp = d.getTime();
-									String[] splittedParts = line.split(".*"+tag+" *\\(.+\\): ");
-									if (splittedParts.length > 0)
-										line = splittedParts[1];
-									else
-										line = "";
-
-									boolean takeLog = false;
-
-									if (lastTimestamp < timestamp) {
-										lastTimestamp = timestamp;
-										logsForTimestamp.clear();
-										logsForTimestamp.add(log);
-										takeLog = true;
-									} else if (lastTimestamp == timestamp && !logsForTimestamp.contains(log)) {
-										logsForTimestamp.add(log);
-										takeLog = true;
-									}
-
-									if (takeLog && mLogListener != null) {
-										try {
-											mLogListener.onLog(tag, LogType.valueOf(logType), timestamp, log);
-										} catch (IllegalArgumentException e) {
-											// thrown if logType is not expected in the enum LogType
-										}
+										mLogListener.onLog(tag, LogType.valueOf(logType), timestamp, log);
+									} catch (IllegalArgumentException e) {
+										// thrown if logType is not expected in the enum LogType
 									}
 								}
 							}
